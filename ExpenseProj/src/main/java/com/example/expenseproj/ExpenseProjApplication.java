fe1a4d5dfcf0b2bd3531b4f28c54c07947e6ad63
obahhur <60160022+obahhur@users.noreply.github.com>
2024-11-04 11:42:31 -0500

    Initial Proj Setup

---
+++ ExpenseProj/src/main/java/com/example/expenseproj/ExpenseProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseProjApplication.class, args);
+    }
+
+}
---
+++ ExpenseProj/src/test/java/com/example/expenseproj/ExpenseProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
